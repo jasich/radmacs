959a5cc37489fd75b0b68efef1e12ee13cf54ed0
Jason Sich <jasich@gmail.com>
2018-03-26 11:50:51 -0400

    Adds copy file name command

--- customizations/files.el
+++ customizations/files.el
@@ -97,3 +97,14 @@
 
 ;; full path in title bar
 (setq-default frame-title-format "%b (%f)")
+
+
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
